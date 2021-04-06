2b1c8688b5e24cf71305d556094145292134834b
rcbdelarosa <rcb.delarosa@gmail.com>
2021-04-05 22:23:25 -0500

    carousel mostly finished

---
+++ src/components/Carousel.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import Typography from "@material-ui/core/Typography";
+import Carousel from 'react-material-ui-carousel';
+import { Paper, Button } from '@material-ui/core';
+
+function Example(props)
+{
+    const items = [
+        {
+            title: "Welcome to BLVCKISKING",
+            description: "This is a project to raise awareness. To make you aware. Aware that Black Is King."
+        },
+        {
+            title: "Why BLVCKISKING?",
+            description: "Inspired by Queen B's 'Black Is King' film, a labor of love, a visual companion of the 2019 Lion King Movie. This site is meant to be a visual exercise that Black people existed long before slavery, persevered throughout and are stepping into greatness in our present. Your existence is radical."
+        },
+    ]
+
+
+    return (
+        <Carousel>
+            {
+                items.map( (item, i) => <Item key={i} item={item} /> )
+            }
+        </Carousel>
+    )
+}
+export default Example;
+
+function Item(props)
+{
+    return (
+        <Paper>
+            <Typography
+                color="textPrimary"
+                gutterBottom
+                variant="h2"
+                align="center"
+                noWrap={true}
+            >
+                {props.item.title}
+            </Typography>
+
+            <p>{props.item.description}</p>
+
+            <Button className="CheckButton">
+                Step into History
+            </Button>
+        </Paper>
+    )
+}
